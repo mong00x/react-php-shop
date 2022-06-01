8a64eeec34016e4f01973c628815bb873aaeae10
Meng Yuan <13714858@student.uts.edu.au>
2022-06-01 22:10:33 +0930

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import $ from "jquery";
+import "./App.css";
+
+function App() {
+  const [name, setName] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const form = $(e.target);
+    $.ajax({
+      type: "POST",
+      url: form.attr("action"),
+      data: form.serialize(),
+      success(data) {
+        setResult(data);
+      },
+    });
+  };
+
+  return (
+    <div className="App">
+      <form
+        action="http://localhost:8000/server.php"
+        method="post"
+        onSubmit={(event) => handleSumbit(event)}
+      >
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(event) => handleChange(event)}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <h1>{result}</h1>
+    </div>
+  );
+}
+
+export default App;
